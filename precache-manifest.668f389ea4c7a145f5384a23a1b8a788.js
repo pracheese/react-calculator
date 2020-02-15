b80c8bfb85f2239cc5c6cc7cbe5e0417b5c837aa
pracheese <prachiverma10@gmail.com>
2020-02-15 17:55:53 -0500

    Updates

---
+++ precache-manifest.668f389ea4c7a145f5384a23a1b8a788.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41180de7ff2beb81b1ef2f988db87657",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "f752392df21a7817b861",
+    "url": "/react-calculator/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d64b6cf3c90621869ce8",
+    "url": "/react-calculator/static/css/main.9d76c179.chunk.css"
+  },
+  {
+    "revision": "f752392df21a7817b861",
+    "url": "/react-calculator/static/js/2.db32ddd3.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/react-calculator/static/js/2.db32ddd3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d64b6cf3c90621869ce8",
+    "url": "/react-calculator/static/js/main.36371a3b.chunk.js"
+  },
+  {
+    "revision": "909d0137fa7114c15e08",
+    "url": "/react-calculator/static/js/runtime-main.a14a8bab.js"
+  }
+]);
